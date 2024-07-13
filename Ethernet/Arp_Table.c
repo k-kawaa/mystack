059d586ead556f06248d6b9372d270eb51d66588
k-kawaa <kaisei.kawashima@gmail.com>
2024-07-13 12:33:27 +0900

    ChangeTable_MAC and AddTable implemented

--- Ethernet/Arp_Table.c
+++ Ethernet/Arp_Table.c
@@ -1,18 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+#include <unistd.h>
 
 #include "Arp_Table.h"
 
 
-int Init_Table(){
+arp_table *Init_Table(){
     arp_table *table;
     table = malloc(sizeof(arp_table));
 
     if(table == NULL){
         puts("Memory allocation error");
-        return -1;
+        return NULL;
     }
     
+    return table;
 }
 
 int AddTable(arp_table *head,arp_table *table){
@@ -20,7 +24,17 @@ int AddTable(arp_table *head,arp_table *table){
     pos = head;
     do{
         pos = pos->next;
-    }while(pos->next != NULL)
+    }while(pos->next != NULL);
     pos->next = table;
+    return 0;
+}
+
+int ChangeTable_MAC(arp_table *head,__uint16_t *dst[4],__uint16_t *mac[6]){
+    arp_table *pos;
+    pos = head;
+    do{
+        pos = pos->next;
+    }while(memcmp(&(head->dst),dst,sizeof(dst)) != 0);
+    memcpy(&(pos->mac),mac,sizeof(mac));
     return 0;
 }
